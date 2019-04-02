3c7f7be953361be37d1d48fd38fe67626ea2705b
Eryn <johnmary1010@gmail.com>
2019-04-02 16:23:35 -0500

    finalized map with regular gas and alternative fuel stations. added the getjson for the mygasfeed api

--- public/js/map.js
+++ public/js/map.js
@@ -2,8 +2,11 @@ require([
   "esri/widgets/Track",
   "esri/layers/FeatureLayer",
   "esri/Map",
-  "esri/views/MapView"
-], function (Track, FeatureLayer, Map, MapView) {
+  "esri/views/MapView",
+  "esri/tasks/Locator",
+  "esri/Graphic"
+  
+], function (Track, FeatureLayer, Map, MapView, Locator, Graphic) {
 
 
   var map = new Map({
@@ -40,51 +43,8 @@ require([
   });
   map.add(cityRender);
 
-  var renderer = {
-    type: "unique-value",  // autocasts as new UniqueValueRenderer()
-    field: "Fuel_Type",
-    defaultSymbol: { 
-      type: "simple-fill",
-      "color": "green" 
-    },  // autocasts as new SimpleFillSymbol()
-    uniqueValueInfos: [{
-      // All features with value of "North" will be blue
-      value: "ELEC",
-      symbol: {
-        type: "simple-fill",  // autocasts as new SimpleFillSymbol()
-        color: "blue"
-      }
-    }, {
-      // All features with value of "East" will be green
-      value: "CNG",
-      symbol: {
-        type: "simple-fill",  // autocasts as new SimpleFillSymbol()
-        color: "yellow"
-      }
-    }, {
-      // All features with value of "South" will be red
-      value: "LPG",
-      symbol: {
-        type: "simple-fill",  // autocasts as new SimpleFillSymbol()
-        color: "orange"
-      }
-    }, {
-      // All features with value of "West" will be yellow
-      value: "BD",
-      symbol: {
-        type: "simple-fill",  // autocasts as new SimpleFillSymbol()
-        color: "pink"
-      }
-    }]
-  };
 
 
-  var markerLayer = new FeatureLayer({
-    url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/ArcGIS/rest/services/Non_Gasoline_Alternative_Fueling_Stations_2019/FeatureServer/0",
-    renderer: renderer,
-  });
-
-  map.add(markerLayer, 1);
 
   //*** ADD ***//
   // Define a popup for Trailheads
@@ -96,10 +56,80 @@ require([
   // Create the layer and set the popup
   var stationLayer = new FeatureLayer({
     url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/ArcGIS/rest/services/Non_Gasoline_Alternative_Fueling_Stations_2019/FeatureServer/0",
-    outFields: ["Station_Na", "Street_Add", "City", "State", "Zip, Owner_Type, Fuel_Type, Station_Ph"],
+    outFields: ["Station_Na", "Street_Add", "City", "State"],
     popupTemplate: stationCard
   });
 
   // Add the layer
   map.add(stationLayer, 2);
+// search for places
+  var places = ["Gas station"];
+
+  var select = document.createElement("select","");
+  places.forEach(function(p){
+    var option = document.createElement("option");
+    option.value = p;
+    option.innerHTML = p;
+    select.appendChild(option);
+  });
+
+  var locator = new Locator({
+    url: "http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer"
+ });
+
+ // Find places and add them to the map
+ function findPlaces(category, pt) {
+  locator.addressToLocations({
+    location: pt,
+    categories: [category],
+    maxLocations: 125,
+    outFields: ["Place_addr", "PlaceName"]
+  })
+  .then(function(results) {
+    // Clear the map
+    view.popup.close();
+    view.graphics.removeAll();
+    // Add graphics
+    results.forEach(function(result){
+      view.graphics.add(
+        new Graphic({
+          attributes: result.attributes,
+          geometry: result.location,
+          symbol: {
+           type: "simple-marker",
+           color: "#000000",
+           size: "10px",
+           outline: {
+             color: "#ffffff",
+             width: "1.5px"
+           }
+          },
+          popupTemplate: {
+            title: "{PlaceName}",
+            content: "{Place_addr}"
+          }
+       }));
+    });
+  });
+}
+
+// Search for places in center of map when the app loads
+findPlaces(select.value, view.center);
+
+// Listen for category changes and find places
+select.addEventListener('change', function (event) {
+  findPlaces(event.target.value, view.center);
+});
+
+// Listen for mouse clicks and find places
+view.on("click", function(event){
+  view.hitTest(event.screenPoint)
+    .then(function(response){
+      if (response.results.length < 2) { // If graphic is not clicked, find places
+        findPlaces(select.options[select.selectedIndex].text, event.mapPoint);
+      }
+  })
+});
+
+  view.ui.add(select, "top-right");
 });
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,23 +3,23 @@ var db = require("../models");
 module.exports = function (app) {
   // Load index page
   app.get("/", function (req, res) {
-      res.render("home", { title: "Cartender - Maintenance Reminders made Easy"})
+    res.render("home", { title: "Cartender - Maintenance Reminders made Easy" })
   });
 
   // app.post("/register", function(req, res) {
   //   console.log(req.body);
   // });
 
-  app.get("/garage", function(req, res) {
+  app.get("/garage", function (req, res) {
     db.vehicle.findAll({}).then((dbvehicle) => {
 
-      res.render("garage", {title: "Cartender - My Garage", dbvehicle});
+      res.render("garage", { title: "Cartender - My Garage", dbvehicle });
     })
   })
 
-  
-  app.get("/map", function(req, res) {
-    res.render("map", {title: "Cartender- My Map"})
+
+  app.get("/map", function (req, res) {
+    res.render("map", { title: "Cartender- My Map" })
   })
 
   // Load example page and pass in an example by id
